d17a1a4cb6f79fb0107b85680b3117e48f3d9285
patvincent21 <lpatvincent21@gmail.com>
2026-07-13 14:32:26 +0800

    Add SAPUI5 HelloWorld app (manifest, view, controller, mockdata)

---
+++ HelloWorld/sapui5-app/webapp/controller/Main.controller.js
@@ -0,0 +1,8 @@
+sap.ui.define(["sap/ui/core/mvc/Controller"], function (Controller) {
+  "use strict";
+  return Controller.extend("HelloWorldSAPUI5.controller.Main", {
+    onInit: function () {
+      // Model is loaded in index.html (mockdata.json). Controller kept minimal.
+    }
+  });
+});
